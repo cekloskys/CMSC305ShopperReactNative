140e3f147ecef2bc296ed0d64df07ba0cb3e1a50
Sue Ceklosky <cekloskys@chc.edu>
2022-09-14 06:45:27 -0400

    Sep 12 2022

--- App.js
+++ App.js
@@ -11,7 +11,14 @@ import React from 'react';
 import type {Node} from 'react';
 import Router from './src/navigation/Router';
 
+const db = require('./src/components/Handlers/database.js');
+
 const App: () => Node = () => {
+  try {
+    db.createListsTable();
+  } catch (error) {
+    console.log('Failed to create lists table ' + error);
+  }
   return <Router />;
 };
 
